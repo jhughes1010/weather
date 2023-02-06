e1edc50626310f20f3353bbf090a52e86fb643cc
tkoeberl <t.koeberlein@gmx.de>
2023-02-06 21:59:30 +0100

    added home assistant auto discovery

--- mqtt.ino
+++ mqtt.ino
@@ -1,10 +1,12 @@
 //mqtt data send
-
-
-
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+struct config {
+  char name[25];
+  char device_class[25];
+  char unit_of_measurement[20];
+};
 
 //=======================================================================
 //  SendDataMQTT: send MQTT data to broker with 'retain' flag set to TRUE
@@ -15,8 +17,6 @@ void SendDataMQTT (struct sensorData *environment)
   char bufferTempC[5];
   char bufferRain[10];
   char bufferRain24[10];
-
-
   int hourPtr = timeinfo.tm_hour;
   client.setServer(mqttServer, mqttPort);
   //client.setCallback(callback);
@@ -35,38 +35,117 @@ void SendDataMQTT (struct sensorData *environment)
       delay(1000);
     }
   }
-  MQTTPublish("boot/", (int)bootCount, true);
-  MQTTPublish("rssi/", rssi, true);
-  MQTTPublish("temperatureF/", (int)environment->temperatureF, true);
-  MQTTPublish("temperatureC/", (int)environment->temperatureC, true);
-  MQTTPublish("windSpeed/", environment->windSpeed, true);
-  MQTTPublish("windDirection/", (int)environment->windDirection, true);
-  MQTTPublish("windCardinalDirection/", environment->windCardinalDirection, true);
-  MQTTPublish("photoresistor/", (int)environment->photoresistor, true);
+  if (discovery_enable){
+    struct config sensor[22]={
+      {"boot", "","#"},
+      {"rssi", "","#"},
+      {"temperatureF", "temperature","°F"},
+      {"temperatureC", "temperature","°C"},
+      {"windSpeed", "wind_speed",""}, 
+      {"windDirection", "",""},
+      {"windCardinalDirection", "",""},
+      {"photoresistor", "",""},
+      {"rainfallInterval", "",""},
+      {"rainfall", "volume","L"},
+      {"rainfall24", "volume","L"},
+      {"batteryVoltage", "voltage","V"},
+      {"lux", "illuminance","lx"},
+      {"UVIndex", "",""},
+      {"relHum", "","%"},
+      {"pressure", "",""},
+      {"caseTemperature", "temperature","°C"},
+      {"batteryADC", "",""},
+      {"ESPcoreF", "temperature","°F"},
+      {"ESPcoreC", "temperature","°C"},
+      {"timeEnabled", "",""},
+      {"lowBattery", "",""},
+    };
+    for (int i=0;i<sizeof sensor/sizeof sensor[0];i++){
+      configMQTT_HA(sensor[i]);
+    };
+  }
+  MQTTPublish("boot/state", (int)bootCount, true);
+  MQTTPublish("rssi/state", rssi, true);
+  MQTTPublish("temperatureF/state", (int)environment->temperatureF, true);
+  MQTTPublish("temperatureC/state", (int)environment->temperatureC, true);
+  MQTTPublish("windSpeed/state", environment->windSpeed, true);
+  MQTTPublish("windDirection/state", (int)environment->windDirection, true);
+  MQTTPublish("windCardinalDirection/state", environment->windCardinalDirection, true);
+  MQTTPublish("photoresistor/state", (int)environment->photoresistor, true);
 #ifndef METRIC
-  MQTTPublish("rainfallInterval/", (float) (rainfall.intervalRainfall * 0.011), true);
-  MQTTPublish("rainfall/", (float) (rainfall.hourlyRainfall[hourPtr] * 0.011), true);
-  MQTTPublish("rainfall24/", (float) (last24() * 0.011), true);
+  MQTTPublish("rainfallInterval/state", (float) (rainfall.intervalRainfall * 0.011), true);
+  MQTTPublish("rainfall/state", (float) (rainfall.hourlyRainfall[hourPtr] * 0.011), true);
+  MQTTPublish("rainfall24/state", (float) (last24() * 0.011), true);
 #else
-  MQTTPublish("rainfallInterval/", (float) (rainfall.intervalRainfall * 0.011 * 25.4), true);
-  MQTTPublish("rainfall/", (float) (rainfall.hourlyRainfall[hourPtr] * 0.011 * 25.4), true);
-  MQTTPublish("rainfall24/", (float) (last24() * 0.011 * 25.4), true);
+  MQTTPublish("rainfallInterval/state", (float) (rainfall.intervalRainfall * 0.011 * 25.4), true);
+  MQTTPublish("rainfall/state", (float) (rainfall.hourlyRainfall[hourPtr] * 0.011 * 25.4), true);
+  MQTTPublish("rainfall24/state", (float) (last24() * 0.011 * 25.4), true);
 #endif
 
-  MQTTPublish("batteryVoltage/", environment->batteryVoltage, true);
-  MQTTPublish("lux/", environment->lux, true);
-  MQTTPublish("UVIndex/", environment->UVIndex, true);
-  MQTTPublish("relHum/", environment->humidity, true);
-  MQTTPublish("pressure/", environment->barometricPressure, true);
-  MQTTPublish("caseTemperature/", environment->BMEtemperature, true);
-  MQTTPublish("batteryADC/", (int)environment->batteryADC, true);
-  MQTTPublish("ESPcoreF/", (int)environment->coreF, true);
-  MQTTPublish("ESPcoreC/", (int)environment->coreC, true);
-  MQTTPublish("timeEnabled/", (int)elapsedTime, true);
-  MQTTPublish("lowBattery/", lowBattery, true);
+  MQTTPublish("batteryVoltage/state", (float) environment->batteryVoltage, true);
+  MQTTPublish("lux/state", environment->lux, true);
+  MQTTPublish("UVIndex/state", environment->UVIndex, true);
+  MQTTPublish("relHum/state", environment->humidity, true);
+  MQTTPublish("pressure/state", environment->barometricPressure, true);
+  MQTTPublish("caseTemperature/state", environment->BMEtemperature, true);
+  MQTTPublish("batteryADC/state", (int)environment->batteryADC, true);
+  MQTTPublish("ESPcoreF/state", (int)environment->coreF, true);
+  MQTTPublish("ESPcoreC/state", (int)environment->coreC, true);
+  MQTTPublish("timeEnabled/state", (int)elapsedTime, true);
+  MQTTPublish("lowBattery/state", lowBattery, true);
   MonPrintf("Issuing mqtt disconnect\n");
   client.disconnect();
   MonPrintf("Disconnected\n");
+}
+
+void configMQTT_HA( struct config sensor)
+{
+  // homeassistant/sensor/weatherA/temperatureC
+  // homeassistant/sensor/weather/temperatureC
+  // {"device_class": "temperature", "name": "temperatureC", "state_topic": "weather/temperatureC", "unit_of_measurement": "°C" }
+  Serial.println("configMQTT_HA");
+  char orgmainTopic[20];
+  char confJson[300];
+  char topic[60];
+  memcpy( orgmainTopic, mainTopic, sizeof(orgmainTopic));
+  memcpy( mainTopic, "homeassistant/", sizeof(mainTopic));
+  char const *v_part1="{\"device_class\": \"";
+  
+
+  char const *v_part5="temperatureC\", \"unit_of_measurement\": \"°C\" }";
+  
+  
+  strcpy(confJson, "{");
+  if(strlen(sensor.device_class) != 0) {
+    strcat(confJson, "\"device_class\": \"");
+    strcat(confJson, sensor.device_class );
+    strcat(confJson, "\",");
+  }
+  strcat(confJson, " \"name\": \"" );
+  strcat(confJson, sensor.name );
+  strcat(confJson, "\", \"state_topic\": \"" );
+  strcat(confJson, orgmainTopic);
+  strcat(confJson, sensor.name);
+  strcat(confJson, "/state\"");
+  //strcat(confJson, "");
+  if(strlen(sensor.unit_of_measurement) != 0) {
+    strcat(confJson, ", \"unit_of_measurement\": \"");
+    strcat(confJson, sensor.unit_of_measurement);
+    strcat(confJson, "\"");
+  } 
+  strcat(confJson, "}");
+
+
+  strcpy(topic, "sensor/");
+  strcat(topic, orgmainTopic);
+  strcat(topic, sensor.name);
+  strcat(topic, "/config");
+  Serial.println(topic);
+  Serial.println(confJson);
+  MQTTPublish(topic,confJson, true);
+
+  memcpy( mainTopic, orgmainTopic, sizeof(mainTopic));
+
 }
 
 //=======================================================================
